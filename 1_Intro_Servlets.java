ad9ee175a57bb6fc8469cf5af09caad98398544d
unstoppablesahilsaifi <97735834+unstoppablesahilsaifi@users.noreply.github.com>
2022-08-25 13:16:58 +0530

    Update 1_Intro_Servlets.java

--- 1_Intro_Servlets.java
+++ 1_Intro_Servlets.java
@@ -1,1 +1,7 @@
-//Test
+/* Servlet is a simple java program that runs on the server and capable of handling the request and generating dynamic response.
+  Client(Chrome, Edge)-------------------------------------------> Server
+                      <------------------------------------------
+  Suppose browser pe hmne search kiya javatpoint.com to ye client ki side s ek request jaygi server pe(Mtlb home page access hoga) lekin website to dynamic h.
+  To jaise hi server pe request jaygi to server pe java program hoga jo request ko accept krega or jdbc k through database s baat krke response generate krdega
+  in the form of HTML kyuki browser ko html hi samjh m aati h.
+  */
