02ffdcd5f56de37456036cd33a7a275a4f1cda43
unstoppablesahilsaifi <97735834+unstoppablesahilsaifi@users.noreply.github.com>
2022-09-02 15:59:09 +0530

    Update 1_Intro_Servlets.java

--- 1_Intro_Servlets.java
+++ 1_Intro_Servlets.java
@@ -18,5 +18,14 @@ To check ki server run ho rha h ya nhi, apne browser per localhost:9494 krke ent
 Configure Apache server with Netbeans
 URL- https://www.youtube.com/watch?v=hbzL22lJpp8&list=PL0zysOflRCel5BSXoslpfDawe8FyyOSZb&index=3
 
-
-  
+// Test Project
+--> If the below error comes...........
+/*In-place deployment at C:\Users\lostl\OneDrive\Documents\NetBeansProjects\Project3\build\web
+Deployment is in progress... 
+deploy?config=file%3A%2FC%3A%2FUsers%2Flostl%2FAppData%2FLocal%2FTemp%2Fcontext7180600714574256491.xml&path=/Project3
+FAIL - Failed to deploy application at context path [/Project3] C:\Users\lostl\OneDrive\Documents\NetBeansProjects\Project3\nbproject\build-impl.xml:1058:
+The module has not been deployed. See the server log for details. BUILD FAILED (total time: 0 seconds) */
+ 
+Solution--> 
+Check out the Windows Services (Services App) and stop the Apache Tomcat if running, then from Netbeans menu select Run->Run Project.
+Netbean will start Tomcat and deploy your web app onto the Tomcat server.
