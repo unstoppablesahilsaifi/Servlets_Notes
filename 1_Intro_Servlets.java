e2a450c3b41f1cbb271d68f783eb4729c4e39c6d
unstoppablesahilsaifi <97735834+unstoppablesahilsaifi@users.noreply.github.com>
2022-09-02 11:40:06 +0530

    Update 1_Intro_Servlets.java

--- 1_Intro_Servlets.java
+++ 1_Intro_Servlets.java
@@ -5,3 +5,17 @@
   To jaise hi server pe request jaygi to server pe java program hoga jo request ko accept krega or jdbc k through database s baat krke response generate krdega
   in the form of HTML kyuki browser ko html hi samjh m aati h.
   */
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Installing Apache tomcat
+Servlet ko install and deploy krne s pehle hme ek server ko install krna pdega
+// To Downlaod Tomcat server-  https://tomcat.apache.org/download-90.cgi
+Port number is 9494 kyuki jo port number hm yha per assign krenge us port number per isi system per koi or server install nhi hona chahiye.
+Apache install krte time role m comma lgakr manager-script add krna hota h.
+To check ki server run ho rha h ya nhi, apne browser per localhost:9494 krke enter krdo to tomcat ka interface aa jayga means run ho rha h.
+  
+
+
+
+  
+  
