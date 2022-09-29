e030b6552da6447991e7c90b852157658eb0be7b
unstoppablesahilsaifi <97735834+unstoppablesahilsaifi@users.noreply.github.com>
2022-09-29 15:32:09 +0530

    Update 5_Cookies_1.java

--- 5_Cookies_1.java
+++ 5_Cookies_1.java
@@ -1,1 +1,27 @@
-Test
+// Coockies
+-> Coockies are the textual information which are stored in key value pair format to the client's browser during multiple requests.
+  (To manage state).
+// Explaination
+-> Jab client server ko koi request send krta h to client every request ko as a new request treat krta h.
+  When we use cookies---->>>>
+  Jab koi client koi request send krega and server us client ki request ko serve krega and jab server hmara response bhej rha hoga to server response k sath kuch data 
+  rakh dega here data means textual information,,,, or usi textual information ko hm coockies bolte h. fir ye coockie jaati h client ki machine m and broser m save ho
+  jaati h, and jab client fir ye koi request2 send krega server per to us request k sath hmari jo coockie thi vo bhi chali jaygi or server us request s coockie ko fetch
+  kar sakta h or pehchan jayga ki bhai ye vhii bnda h jisne pehle request send ki thi.
+  
+  
+  // How to use cookies in Java
+  In order to use cookies in java, there is a  Coockie class in java  present in javax.servlet.http package.
+  To make coockie just create a object of  Coockie class and pass name and its value.
+  
+  ex- Coockie c=new Coockie("name","username");// key value pass krni hoti h
+      response.addCoockie(c);// response m add krdete h.
+
+// How to add coockie in response
+To add coockie in response just use addCoockie(Coockie) method of response interface.
+  
+// How to fetch coockie
+obvious h ki ab coockie request k sath aa rhi hogi to hme vhi s fetch krni h.
+ Coockie c[]=request.getCoockie();
+// isme s coockie nikalkr ek array m store kr lenge. agar kuch nhi hoga coockie to array m NULL hoga.
+  
